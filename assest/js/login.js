b34283936c06e7210b127607cfd59373fb8411bf
thuy trang <leethuytrang2605@gmail.com>
2023-08-23 15:36:18 +0700

    đã chỉnh login & introduce => chỉnh lại icon footer

--- assest/js/login.js
+++ assest/js/login.js
@@ -1,18 +1,17 @@
-
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+const formal = document.querySelector(".formal");
 const sign_in_btn2 = document.querySelector("#sign-in-btn2");
 const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+  formal.classList.add("sign-up-mode");
 });
 sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+  formal.classList.remove("sign-up-mode");
 });
 sign_up_btn2.addEventListener("click", () => {
-  container.classList.add("sign-up-mode2");
+  formal.classList.add("sign-up-mode2");
 });
 sign_in_btn2.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode2");
+  formal.classList.remove("sign-up-mode2");
 });
