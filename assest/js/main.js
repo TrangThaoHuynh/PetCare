021c6efa1edaa572ed3e427740496e8d5c9a8f92
Trang Thao <httrang2004@gmail.com>
2023-08-23 23:23:44 +0700

    sua

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,17 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //Lấy thẻ HTML có id là "hamburger-menu" và "nav-menu"
-  //và lưu vào biến `hamburgermenu` và `navmenu`
-  const hamburgerMenu = document.querySelector("#hamburger-menu");
-  const navMenu = document.querySelector("#nav_menu");
-  //Gán một event listener cho nút "hamburger-menu" để xử lý sự kiện click.
-  hamburgerMenu.addEventListener("click", () => {
-    // //Callback function được gọi khi click xảy ra.
-    hamburgerMenu.classList.toggle("active");
-    //Trong callback, class "active" được thêm hoặc xóa khỏi `hamburgermenu`
-    navMenu.classList.toggle("active");
-    //và `navmenu` bằng cách sử dụng `classList.toggle()`
-  });
-});
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+   navToggler.classList.toggle("active");
+   const nav = document.querySelector(".menu");
+   nav.classList.toggle("open");
+   if(nav.classList.contains("open")){
+     nav.style.maxHeight = nav.scrollHeight + "px";
+   }
+   else{
+     nav.removeAttribute("style");
+   }
+} 
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   //Lấy thẻ HTML có id là "hamburger-menu" và "nav-menu"
+//   //và lưu vào biến `hamburgermenu` và `navmenu`
+//   const hamburgerMenu = document.querySelector("#hamburger-menu");
+//   const navMenu = document.querySelector("#nav_menu");
+//   //Gán một event listener cho nút "hamburger-menu" để xử lý sự kiện click.
+//   hamburgerMenu.addEventListener("click", () => {
+//     // //Callback function được gọi khi click xảy ra.
+//     hamburgerMenu.classList.toggle("active");
+//     //Trong callback, class "active" được thêm hoặc xóa khỏi `hamburgermenu`
+//     navMenu.classList.toggle("active");
+//     //và `navmenu` bằng cách sử dụng `classList.toggle()`
+//   });
+// });
 
 // ĐẾM SỐ
 const numbers = [
